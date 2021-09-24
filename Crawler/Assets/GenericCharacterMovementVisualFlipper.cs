6978dd63b8818bfe0d056bd2db89b417c93c2d2b
Patrik Kumpula <patrik19100@student.hamk.fi>
2021-09-24 07:25:51 +0300

    Mookit flippaa nyt oikeaan suuntaan liikkuessaan

---
+++ Crawler/Assets/GenericCharacterMovementVisualFlipper.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericCharacterMovementVisualFlipper : MonoBehaviour
+{
+    SpriteRenderer m_SpriteRenderer;
+    Vector3 lastFramePos = Vector3.zero;
+    bool flippedOnXAxis = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //So, if the character was further right last frame, meaning the character wandered left...
+        if (lastFramePos.x > transform.position.x)
+        {
+            //Also check supporting bool to reduce calls to change spriterenderer flip direction
+            if (!flippedOnXAxis)
+            {
+                flippedOnXAxis = true;
+                m_SpriteRenderer.flipX = true;
+            }
+
+        }
+        else if (lastFramePos.x < transform.position.x)
+        {
+            if (flippedOnXAxis)
+            {
+                flippedOnXAxis = false;
+                m_SpriteRenderer.flipX = false;
+            }
+
+        }
+    }
+    private void LateUpdate()
+    {
+        lastFramePos = transform.position;
+    }
+}
