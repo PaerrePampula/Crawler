29f484c27585aa03a344e9bebd2b772c69471cbb
Patrik Kumpula <patrik19100@student.hamk.fi>
2021-09-24 08:27:42 +0300

    Tutoriaali myyjästä + paremmin luettavissa oleva pelaajan puhe

--- Crawler/Assets/Seller.cs
+++ Crawler/Assets/Seller.cs
@@ -2,8 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seller : MonoBehaviour
+public class Seller : MonoBehaviour, IPlayerInteractable
 {
+    [SerializeField] string buyItem;
+    public void DoPlayerInteraction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public string getPlayerInteractionString()
+    {
+        return buyItem;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
