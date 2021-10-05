32d3799cd297b6ec52d46bb5e27e40fa060be6ea
Patrik Kumpula <patrik19100@student.hamk.fi>
2021-10-05 13:24:22 +0300

    Kuvitteellinen commit, mene pois!
    Tämä EI lisää easteregiä, jos pelaajalla on  enemmän kuin 75% lisää movespeediä

---
+++ Crawler/Assets/EurobeatDj.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// You see nothing!
+/// </summary>
+public class EurobeatDj : MonoBehaviour
+{
+    float currentValue = 0;
+    float neededvalue = 0.75f;
+    bool doriftoTime;
+    AudioSource innocentAudioSource;
+    [SerializeField] CharacterController Takumi;
+    private void Start()
+    {
+        innocentAudioSource = GetComponent<AudioSource>();
+        Player.Singleton.AddDelegateOnStatBuff(StatType.MovementSpeed, checkIfSpeedIsEnoughForSomeFunnies);
+    }
+
+    private void checkIfSpeedIsEnoughForSomeFunnies(float value)
+    {
+        currentValue += value;
+        if (!doriftoTime)
+        {
+            if (currentValue >= neededvalue)
+            {
+                doriftoTime = true;
+                innocentAudioSource.Play();
+                StartCoroutine(AiActionWaiter.actionWait(() => SayDorifto(), Time.time + 3f));
+            }
+        }
+
+
+    }
+
+    private void SayDorifto()
+    {
+
+        PlayerController.Singleton.GetComponentInChildren<CharacterTextBox>().InvokeTextDisplay("Is this... " +
+            "MULTI DUNGEON DRIFTING????");
+    }
+
+    private void Update()
+    {
+        if (doriftoTime)
+        {
+            if (Takumi.velocity.magnitude > 0)
+            {
+                innocentAudioSource.volume = Mathf.Clamp(Takumi.velocity.magnitude * 0.02f, 0, 0.5f);
+            }
+            else
+            {
+                innocentAudioSource.volume = 0.02f;
+            }
+        }
+
+
+    }
+}
