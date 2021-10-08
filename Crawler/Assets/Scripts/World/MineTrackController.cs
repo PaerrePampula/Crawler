06cb032ff6749cb8dc670eb092838016ff33ea9d
Patrik Kumpula <patrik19100@student.hamk.fi>
2021-10-08 09:05:44 +0300

    Mainostettu minecart delay pelaa oikein + tulipallot ei lennä loputtomasti

--- Crawler/Assets/Scripts/World/MineTrackController.cs
+++ Crawler/Assets/Scripts/World/MineTrackController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class MineTrackController : MonoBehaviour
 {
+    bool hasStarted = false;
     [SerializeField] ChildOnTrigger triggerToReturnCart;
     [SerializeField] GameObject _minecartPrefab;
-    [SerializeField] float _minecartReturnToStartDelay = 5f;
+    [SerializeField] float _minecartReturnToStartDelay = 2.5f;
     [SerializeField] float _minecartSpeed = 5f;
     [SerializeField] Transform trackStart;
     [SerializeField] Transform trackEnd;
@@ -21,14 +22,43 @@ public class MineTrackController : MonoBehaviour
         //Sub to trigger to return cart, and specify that the cart is need to return any collider transform
         triggerToReturnCart.delegatesOnTrigger = (collider) =>  setCartToStart();
         triggerToReturnCart.predicatesForDelegateTrigger = isCart();
+
+    }
+    private void OnEnable()
+    {
         setCartToStart();
+        //If the player left the room, this will stay true, reset this value in these cases
+        hasStarted = false;
+    }
+    private void OnDisable()
+    {
+        //The coroutine for resetting track might still be running when leaving room
+        StopAllCoroutines();
     }
 
     void setCartToStart()
     {
+        //if no cart has moved, insta start the track movement, else wait for the delay first
+        if (hasStarted)
+        {
+            _cart.gameObject.SetActive(false);
+            StartCoroutine(ActionDelayer.actionWait(() => SetCartToPositionAndReEnable(), Time.time + _minecartReturnToStartDelay));
+        }
+        else
+        {
+            SetCartToPositionAndReEnable();
+            hasStarted = true;
+        }
+
+    }
+
+    private void SetCartToPositionAndReEnable()
+    {
+        _cart.gameObject.SetActive(true);
         _cart.transform.position = trackStart.position;
         cartDirection = (trackEnd.position - trackStart.position).normalized;
     }
+
     // Update is called once per frame
     void Update()
     {
