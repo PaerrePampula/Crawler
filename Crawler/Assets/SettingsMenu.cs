74fbaaa4c1e40e1eacad140b4f940af338abe63d
Leevirasanen <leevi.rasanen@student.hamk.fi>
2021-10-12 23:36:55 +0300

    Muutin pause menun settings kohtaa

--- Crawler/Assets/SettingsMenu.cs
+++ Crawler/Assets/SettingsMenu.cs
@@ -6,6 +6,9 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField] AudioMixer mainMixer;
+    [SerializeField] GameObject settingsMenu;
+
+    bool Settings = false;
 
     public void ToggleWindowMode()
     {
@@ -14,5 +17,19 @@ public class SettingsMenu : MonoBehaviour
     public void SetVolume(float sliderValue)
     {
         mainMixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void ToggleSettingsPanel()
+    {
+        if (Settings)
+        {
+            settingsMenu.SetActive(false);
+            Settings = false;
+        }
+        else
+        {
+            settingsMenu.SetActive(true);
+            Settings = true;
+        }
     }
 }
